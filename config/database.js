9e0647bde628099918885ce2d6380346672f3715
Nguyenkhoi196 <nguyenkhoi196@gmail.com>
2023-09-07 12:45:10 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -91,17 +91,18 @@
 //     },
 //   };
 // };
+
 // strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'KhoiAnh-strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'nguyenkhoi196'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA", "public"),
       // ssl: {
       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       // },
