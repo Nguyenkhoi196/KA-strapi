db4404b9bd29400cdffb43afd34fc8386a056590
Nguyenkhoi196 <nguyenkhoi196@gmail.com>
2023-09-25 09:12:49 +0700

    Update package.json

--- config/database.js
+++ config/database.js
@@ -9,9 +9,9 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
       schema: env("DATABASE_SCHEMA", "public"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      // },
     },
     debug: false,
   },
