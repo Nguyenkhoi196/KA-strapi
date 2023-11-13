41f914a3b578540c71a83f372b83a570253a58b7
Nguyenkhoi196 <nguyenkhoi196@gmail.com>
2023-11-13 14:29:18 +0700

    update database & server connect

--- config/database.js
+++ config/database.js
@@ -3,15 +3,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      schema: env("DATABASE_SCHEMA", "public"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      host: env("DATABASE_HOST_DEV"),
+      port: env.int("DATABASE_PORT_DEV"),
+      database: env("DATABASE_NAME_DEV"),
+      user: env("DATABASE_USERNAME_DEV"),
+      password: env("DATABASE_PASSWORD_DEV"),
+      schema: env("DATABASE_SCHEMA_DEV", "public"),
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      // },
     },
     debug: false,
   },
