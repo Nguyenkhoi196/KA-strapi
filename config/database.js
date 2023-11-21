139ba27e76c3c47c210274157641adba0b16c493
Nguyenkhoi196 <92535681+Nguyenkhoi196@users.noreply.github.com>
2023-11-21 18:54:45 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,12 +3,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST_DEV"),
-      port: env.int("DATABASE_PORT_DEV"),
-      database: env("DATABASE_NAME_DEV"),
-      user: env("DATABASE_USERNAME_DEV"),
-      password: env("DATABASE_PASSWORD_DEV"),
-      schema: env("DATABASE_SCHEMA_DEV", "public"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAMEV"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA", "public"),
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
