9c67e04b9edf99d7fdcb8c03336294dd7becc235
Nguyenkhoi196 <92535681+Nguyenkhoi196@users.noreply.github.com>
2023-11-21 18:53:40 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -9,9 +9,20 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME_DEV"),
       password: env("DATABASE_PASSWORD_DEV"),
       schema: env("DATABASE_SCHEMA_DEV", "public"),
-      // ssl: {
-      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      // },
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
+    },
+       acquireConnectionTimeout: 1000000,
+    pool: {
+      min: 0,
+      max: 4,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 300000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis:1000,
+      createRetryIntervalMillis: 2000
     },
     debug: false,
   },
