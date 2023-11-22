aa538de3c5014f3dc051c7cfdea95791a92153c6
Nguyenkhoi196 <nguyenkhoi196@gmail.com>
2023-11-22 09:41:18 +0700

    fix connection timeout

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -11,6 +11,17 @@ module.exports = ({ env }) => ({
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
+      acquireConnectionTimeout: 1000000,
+      pool: {
+        min: 0,
+        max: 4,
+        acquireTimeoutMillis: 300000,
+        createTimeoutMillis: 300000,
+        destroyTimeoutMillis: 300000,
+        idleTimeoutMillis: 30000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 2000,
+      },
     },
     debug: false,
   },
