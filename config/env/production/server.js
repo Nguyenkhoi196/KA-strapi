e4e57972d21747c842f69dabc3c759b5a06139bf
Nguyenkhoi196 <nguyenkhoi196@gmail.com>
2023-11-22 09:21:59 +0700

    Update folder save imgae, server in production

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
-  }
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
 });
