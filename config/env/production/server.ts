75f7b936a6aa3542bf1acd6285a4fdb9c19756ff
Nguyenkhoi196 <nguyenkhoi196@gmail.com>
2024-01-09 17:00:51 +0700

    update config database

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
+  // url: env('STRAPI_URL', "https://khoianh-strapi.onrender.com"),
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1338),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
