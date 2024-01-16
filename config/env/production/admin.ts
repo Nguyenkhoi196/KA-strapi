59f67f580988e9cc0c7960315fea2ed2d6b62b0d
Nguyenkhoi196 <nguyenkhoi196@gmail.com>
2024-01-16 16:53:12 +0700

    update admin production

--- config/env/production/admin.ts
+++ config/env/production/admin.ts
@@ -1,7 +1,18 @@
 export default ({ env }) => ({
   url: env("STRAPI_ADMIN_URL", '/admin'),
   auth: {
-    secret: env("ADMIN_JWT_SECRET"),
+    events: {
+      onConnectionSuccess(e) {
+        console.log(e.user, e.provider);
+      },
+      onConnectionError(e) {
+        console.error(e.error, e.provider);
+      },
+    },
+    options: {
+      expiresIn: '7d',
+    },
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
     salt: env("API_TOKEN_SALT"),
@@ -10,5 +21,10 @@ export default ({ env }) => ({
     token: {
       salt: env("TRANSFER_TOKEN_SALT"),
     },
+  },
+  serveAdminPanel: env.bool('SERVE_ADMIN', true),
+  forgotPassword: {
+    from: 'no-reply@example.com',
+    replyTo: 'no-reply@example.com',
   },
 });
